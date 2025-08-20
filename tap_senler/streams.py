049a851fd7754f7af977684f0172bd8fb0756d61
DmitreyZl <70319100+DmitreyZl@users.noreply.github.com>
2025-08-20 22:41:31 +0300

    add new stream

--- tap_senler/streams.py
+++ tap_senler/streams.py
@@ -105,6 +105,79 @@ class DeliveriesGet(SenlerStream):
         yield from extract_jsonpath(self.records_jsonpath, input=items)
 
 
+
+class SubscriptionsGet(SenlerStream):
+    """Define custom stream."""
+
+    name = "subscriptions_get"
+    primary_keys = ["subscription_id"]
+    cont = {}
+
+    schema = th.PropertiesList(
+        th.Property(
+            "subscription_id",
+            th.IntegerType,
+            description="The post's system ID",
+        ),
+        th.Property("name", th.StringType),
+        th.Property("school", th.StringType),
+        th.Property("group_id", th.IntegerType)
+    ).to_dict()
+
+    def get_records(
+            self,
+            context: Context | None,
+    ) -> t.Iterable[dict]:
+        """Return a generator of record-type dictionary objects.
+
+        The optional `context` argument is used to identify a specific slice of the
+        stream if partitioning is required for the stream. Most implementations do not
+        require partitioning and should ignore the `context` argument.
+
+        Args:
+            context: Stream partition or context dictionary.
+
+        Raises:
+            NotImplementedError: If the implementation is TODO
+        """
+        # Ваш токен доступа
+        # params = self.config.get("params") or {}
+        token = self.config.get('token')
+        api = Senler(token)
+
+        records = api(
+            methods.Subscriptions.get,
+            vk_group_id=self.config.get('group_id'),
+            count=100
+        )
+
+        items = records['items']
+        offset = 100
+        records_ = {'items': []}
+        while len(items) == 100 or len(records_.get('items')) == 100:
+            records_ = api(
+                methods.Deliveries.get,
+                vk_group_id=self.config.get('group_id'),
+                count=100,
+                offset=offset
+            )
+            items += records_['items']
+            offset += 100
+
+        for i in items:
+            i.update({'school': self.config.get('school'),
+                      'group_id': self.config.get('group_id')})
+
+        self.cont["ids"] = [record.get('subscription_id')
+                            for record in items
+                            ]  # Обновляем состояние
+        # Логируем изменения в контексте
+
+        self.logger.info(f"Updated context: {self.cont}")
+        yield from extract_jsonpath(self.records_jsonpath, input=items)
+
+
+
 class BotsGet(SenlerStream):
     """Define custom stream."""
 
